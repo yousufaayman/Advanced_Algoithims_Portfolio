b8b968edc68af31fbe1e32fc9f4b9a7337055cfd
yousufaayman <yousufaymanh04@gmail.com>
2024-04-27 23:21:30 +0300

    Task_3

--- Task_3.cpp
+++ Task_3.cpp
@@ -8,44 +8,54 @@
 
 using namespace std;
 
-int heapify(int arr[], int size, int index, int& comparisons) {
+int heapify(int arr[], int size, int index, int &comparisons)
+{
     int smallest = index;
     int left = 2 * index + 1;
     int right = 2 * index + 2;
 
-    if (left < size && arr[left] < arr[smallest]) {
+    if (left < size && arr[left] < arr[smallest])
+    {
         smallest = left;
         comparisons++;
     }
-    if (right < size && arr[right] < arr[smallest]) {
+    if (right < size && arr[right] < arr[smallest])
+    {
         comparisons++;
         smallest = right;
     }
-    if (smallest != index) {
+    if (smallest != index)
+    {
         swap(arr[index], arr[smallest]);
         heapify(arr, size, smallest, comparisons);
     }
     return comparisons;
 }
 
-int heapSortCount(int arr[], int size) {
+int heapSortCount(int arr[], int size)
+{
     int comparisons = 0;
     for (int i = size / 2 - 1; i >= 0; i--)
         heapify(arr, size, i, comparisons);
 
-    for (int i = size - 1; i > 0; i--) {
+    for (int i = size - 1; i > 0; i--)
+    {
         swap(arr[0], arr[i]);
         heapify(arr, i, 0, comparisons);
     }
     return comparisons;
 }
 
-int bubbleSortCount(int arr[], int size) {
+int bubbleSortCount(int arr[], int size)
+{
     int comparisons = 0;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
             comparisons++;
-            if (arr[j] > arr[j + 1]) {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
             }
         }
@@ -53,11 +63,14 @@ int bubbleSortCount(int arr[], int size) {
     return comparisons;
 }
 
-int selectionSortCount(int arr[], int size) {
+int selectionSortCount(int arr[], int size)
+{
     int comparisons = 0;
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1; i++)
+    {
         int minIndex = i;
-        for (int j = i + 1; j < size; j++) {
+        for (int j = i + 1; j < size; j++)
+        {
             comparisons++;
             if (arr[j] < arr[minIndex])
                 minIndex = j;
@@ -67,12 +80,15 @@ int selectionSortCount(int arr[], int size) {
     return comparisons;
 }
 
-int insertionSortCount(int arr[], int size) {
+int insertionSortCount(int arr[], int size)
+{
     int comparisons = 0;
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++)
+    {
         int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j = j - 1;
             comparisons++;
@@ -82,19 +98,23 @@ int insertionSortCount(int arr[], int size) {
     return comparisons;
 }
 
-void testComparisons() {
+void testComparisons()
+{
     srand(time(NULL));
     cout << "Sort Type | Heap Sort | Bubble Sort | Selection Sort | Insertion Sort" << endl;
-    
-    for (int testType = 0; testType < 3; testType++) {
+
+    for (int testType = 0; testType < 3; testType++)
+    {
         int arraySize = 30;
         int comparisonCounts[4] = {0};
         chrono::duration<double> totalTimes[4];
 
-        for (int t = 0; t < arraySize; t++) {
+        for (int t = 0; t < arraySize; t++)
+        {
             vector<int> arr(30);
             if (testType == 0)
-                generate(arr.begin(), arr.end(), []() { return rand() % 100; });
+                generate(arr.begin(), arr.end(), []()
+                         { return rand() % 100; });
             else if (testType == 1)
                 iota(arr.begin(), arr.end(), 0);
             else
@@ -126,14 +146,16 @@ void testComparisons() {
         }
 
         cout << ((testType == 0) ? "Random" : ((testType == 1) ? "Sorted" : "Inversely Sorted")) << " Arrays | ";
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++)
+        {
             cout << comparisonCounts[i] / arraySize << " comps, " << totalTimes[i].count() / arraySize << " sec | ";
         }
         cout << endl;
     }
 }
 
-int main() {
+int main()
+{
     testComparisons();
     return 0;
 }
