4004d892d27b651cd79bbfdaa245b9eeaa79cd82
yousufaayman <yousufaymanh04@gmail.com>
2024-04-27 20:12:46 +0300

    formatted document using prettier, set values of duplicate elements to null

--- Task_1.cpp
+++ Task_1.cpp
@@ -11,17 +11,28 @@ void rmDuplicates(char arrayOfChar[], int &lenArrChar)
     sort(arrayOfChar, arrayOfChar + lenArrChar);
 
     int uniqueCount = 0;
+
     for (int i = 0; i < lenArrChar - 1; i++)
     {
         if (arrayOfChar[i] != arrayOfChar[i + 1])
         {
             arrayOfChar[uniqueCount++] = arrayOfChar[i];
         }
+        else
+        {
+            arrayOfChar[i] = '\0';
+        }
     }
+
     arrayOfChar[uniqueCount++] = arrayOfChar[lenArrChar - 1];
+
+    for (int j = uniqueCount; j < lenArrChar; j++)
+    {
+        arrayOfChar[j] = '\0';
+    }
+
     lenArrChar = uniqueCount;
 }
-
 int main()
 {
     char arrayOfChar[] = {'a', 'b', 'a', 'c', 'd', 'b', 'c'};
@@ -31,6 +42,7 @@ int main()
 
     cout << endl
          << "Array After Removing Duplicates: ";
+
     for (int i = 0; i < lenArrChar; i++)
     {
         cout << arrayOfChar[i] << " ";
